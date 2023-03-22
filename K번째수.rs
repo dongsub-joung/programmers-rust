387ad2cca25800bfecbb28ec1f3c7cba7ad3a45c
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-03-22 21:55:34 +0900

    2d

--- K번째수.rs
+++ K번째수.rs
@@ -2,30 +2,23 @@ fn main(){
     
 }
 
-fn init(mut vec_list: Vec<usize>, two_dimention: Vec<Vec<usize>>) -> Vec<usize>{
+fn init(vec_list: Vec<usize>, two_dimention: Vec<Vec<usize>>) -> Vec<usize>{
     let mut result_v= Vec::new();
 
     for vec1 in two_dimention{
         let i= vec1[0].clone();
         let j= vec1[1].clone();
         let k= vec1[2].clone();
-        
+
         let v_slice= &vec1[i..j]; 
+        
+        //v_slice.sort();
 
-        qick_sorting(v_slice);
+        let k_number= v_slice[k];
 
-
-        //let k_number= v_slice[1];
-
-        //result_v.push(k_number);
+        result_v.push(k_number);
     }
     return result_v;
-}
-
-fn qick_sorting(vector: &[usize]) -> Vec<usize>{
-    let mut sorted_vector: Vec<usize>= Vec::new();
-
-    return sorted_vector;
 }
 
 
