d27d75779731dd3b1f07608753d6a539f82df1b2
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-03-23 14:11:33 +0900

    2d-done

--- K번째수.rs
+++ K번째수.rs
@@ -13,7 +13,7 @@ fn init(vec_list: Vec<usize>, two_dimention: Vec<Vec<usize>>) -> Vec<usize>{
         let j= vec1[1].clone();
         let k= vec1[2].clone()-1;
 
-        let mut v_slice= &mut vec_list[i..j].to_vec(); 
+        let mut v_slice= vec_from_slice(vec_list.clone(), i, j);
         
         v_slice.sort();
 
@@ -24,6 +24,9 @@ fn init(vec_list: Vec<usize>, two_dimention: Vec<Vec<usize>>) -> Vec<usize>{
     return result_v;
 }
 
+fn vec_from_slice(vec_list: Vec<usize>, i: usize, j: usize) -> Vec<usize>{
+    vec_list[i..j].to_vec()
+}
 
 #[cfg(test)]
 mod tests{
@@ -31,6 +34,14 @@ mod tests{
 
     #[test]
     fn test1(){
+        let v= vec![1usize, 5, 2, 6, 3, 7, 4];
+    
+        let expect= vec![2,6,3];
+        assert_eq!(expect, vec_from_slice(v, 2, 5))
+    }
+
+    #[test]
+    fn holeTest(){
         let v= vec![1usize, 5, 2, 6, 3, 7, 4];
         let vec_list= vec![vec![2usize, 5, 3], vec![4usize, 4, 1], vec![1usize, 7, 3]];
         let result= vec![5usize, 6, 3];
