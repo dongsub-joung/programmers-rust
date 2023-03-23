4bcd9b4bc022d570cffa8d7734e6e47b8dc08cb5
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-03-23 11:47:14 +0900

    2d

--- K번째수.rs
+++ K번째수.rs
@@ -1,18 +1,21 @@
 fn main(){
-    
+    let v= vec![1usize, 5, 2, 6, 3, 7, 4];
+    let vec_list= vec![vec![2usize, 5, 3], vec![4usize, 4, 1], vec![1usize, 7, 3]];
+    let result= init(v, vec_list);
+
 }
 
 fn init(vec_list: Vec<usize>, two_dimention: Vec<Vec<usize>>) -> Vec<usize>{
     let mut result_v= Vec::new();
 
-    for vec1 in two_dimention{
-        let i= vec1[0].clone();
+    for mut vec1 in two_dimention{
+        let i= vec1[0].clone()-1;
         let j= vec1[1].clone();
-        let k= vec1[2].clone();
+        let k= vec1[2].clone()-1;
 
-        let v_slice= &vec1[i..j]; 
+        let mut v_slice= &mut vec_list[i..j].to_vec(); 
         
-        //v_slice.sort();
+        v_slice.sort();
 
         let k_number= v_slice[k];
 
