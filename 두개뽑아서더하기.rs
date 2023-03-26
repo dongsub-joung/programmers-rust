12f5d2fb85fdb6088a70f1467b596be743aea7e2
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-03-26 10:03:50 +0900

    4d-done

--- 두개뽑아서더하기.rs
+++ 두개뽑아서더하기.rs
@@ -2,10 +2,14 @@ pub fn solution(numbers: Vec<usize>) -> Vec<usize> {
     let mut result_numbers: Vec<usize>= Vec::new();
     let numbers_copy= numbers.clone();
 
-    for number in numbers{
-        for j in 1..numbers_copy.len()-1{
-            let added= number + numbers_copy[j];
-            result_numbers.push(added); 
+    let size= numbers_copy.len();
+    for number in 0..size {
+        for j in number+1..size{
+            let added= numbers_copy[number] + numbers_copy[j];
+            
+            if !result_numbers.contains(&added) {
+                result_numbers.push(added); 
+            }
         }
     }
 
@@ -25,6 +29,7 @@ mod tests {
         assert_eq!(result, vec!{2,3,4,5,6,7});
     }
 
+    #[test]
     fn testB() {
         let result = solution(vec!{5,0,2,7});
         assert_eq!(result, vec!{2,5,7,9,12});
