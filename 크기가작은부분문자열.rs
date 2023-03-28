249da69bbac9385646ee1210c29401fab6594eec
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-03-28 21:09:09 +0900

    5d

--- 크기가작은부분문자열.rs
+++ 크기가작은부분문자열.rs
@@ -1,4 +1,6 @@
-pub fn solution(t: String, p: String) -> usize {
+
+// not work
+fn solution(t: String, p: String) -> usize {
     let mut answer= 0usize;
 
     let p_size= p.len();
@@ -26,6 +28,35 @@ pub fn solution(t: String, p: String) -> usize {
     return answer;
 }
 
+
+fn solution2(t: String, p: String) -> usize {
+    let mut answer= 0usize;
+
+    let p_size= p.len();
+    let p= p.parse::<usize>().unwrap();
+
+    for i in 0..t.len()-p_size+1{
+        let sliced= substring(t.clone(), i, i+p_size);
+        if sliced <= p {
+            answer= answer+1;
+        }
+    }
+    return answer;
+}
+
+fn substring(t: String, frist_idx: usize, second_idx: usize) -> usize{
+    let mut number= 0usize; 
+
+    // let chars: Vec<_>= t.chars().collect();
+    // let mut string_list: Vec<char>= Vec::new();
+
+    let slice= &t[frist_idx..second_idx];
+
+    number= slice.parse::<usize>().unwrap();
+
+    return number;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -35,7 +66,7 @@ mod tests {
         let t= String::from("3141592");
         let p= String::from("271");
 
-        let result = solution(t,p);
+        let result = solution2(t,p);
         assert_eq!(result, 2);
     }
 
@@ -44,7 +75,7 @@ mod tests {
         let t= String::from("500220839878");
         let p= String::from("7");
 
-        let result = solution(t,p);
+        let result = solution2(t,p);
         assert_eq!(result, 8);
     }
 
@@ -53,7 +84,7 @@ mod tests {
         let t= String::from("10203");
         let p= String::from("15");
 
-        let result = solution(t,p);
+        let result = solution2(t,p);
         assert_eq!(result, 3);
     }
 }
